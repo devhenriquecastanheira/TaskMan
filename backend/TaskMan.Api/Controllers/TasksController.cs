45914d35d8ed963b2fb8e0ab4b614d63e94a4e59
Henrique Cesar <henrique.fc18@gmail.com>
2026-08-30 14:00:13 -0300

    Implementando CRUD de TaskItem.

--- backend/TaskMan.Api/Controllers/TasksController.cs
+++ backend/TaskMan.Api/Controllers/TasksController.cs
@@ -30,4 +30,63 @@ public class TasksController : ControllerBase
     {
         return Ok(Tasks);
     }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var task = Tasks.FirstOrDefault(t => t.Id == id);
+
+        if (task is null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(task);
+    }
+
+    [HttpPost]
+    public IActionResult Create(TaskItem newTask)
+    {
+        var maiorId = Tasks.Count > 0
+            ? Tasks.Max(taks => taks.Id)
+            : 0;
+        
+        newTask.Id = maiorId + 1;
+        
+        Tasks.Add(newTask);
+        
+        return CreatedAtAction(nameof(GetById), new { id = newTask.Id }, newTask);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var task = Tasks.FirstOrDefault(task => task.Id == id);
+        
+        if (task is null)
+        {
+            return NotFound();
+        }
+        
+        Tasks.Remove(task);
+        
+        return NoContent();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, TaskItem updatedTask)
+    {
+        var task = Tasks.FirstOrDefault(task => task.Id == id);
+
+        if (task is null)
+        {
+            return NotFound();
+        }
+
+        task.Title = updatedTask.Title;
+        task.Description = updatedTask.Description;
+        task.Status = updatedTask.Status;
+
+        return NoContent();
+    }
 }
