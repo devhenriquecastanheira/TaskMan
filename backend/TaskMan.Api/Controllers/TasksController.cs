ff264ff2a7c1e896c98196696a8eed1583d01087
Henrique Cesar <henrique.fc18@gmail.com>
2026-08-30 13:37:05 -0300

    Criando backend.

---
+++ backend/TaskMan.Api/Controllers/TasksController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskMan.Api.Models;
+
+namespace TaskMan.Api.Controllers;
+
+[ApiController]
+[Route("api/tasks")]
+public class TasksController : ControllerBase
+{
+    private static readonly List<TaskItem> Tasks = 
+    [
+        new()
+        {
+            Id = 1,
+            Title = "Estudar Angular",
+            Description = "Aprender Angular 22",
+            Status = "Pendente"
+        },
+        new()
+        {
+            Id = 2,
+            Title = "Estudar C#",
+            Description = "Aprender ASP.NET Core",
+            Status = "Em andamento"
+        }
+    ];
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(Tasks);
+    }
+}
